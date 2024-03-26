a3a0c46af9aa5c1ed0d25d6de4b872fa07b33529
wichapas <wpichetp@ucsc.edu>
2024-03-26 05:55:25 -0700

    update css

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,92 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <a href="https://github.com/Muffing21" target="_blank">
-      <img width="200" height="200" alt="GitHub Logomark" class="height-auto" src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png">
-    </a>
-    <h1>My Website</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+    <div class="header">
+      <a href="https://github.com/Muffing21" target="_blank">
+        <img width="200" height="200" alt="GitHub Logomark" class="height-auto" src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png">
+      </a>
+      <h1>Wichapas Pichetpongsa</h1>
+      <div class="tabs">
+        <button class="tab-link active" data-tab-id="tab1">Home</button>
+        <button class="tab-link" data-tab-id="tab2">Projects</button>
+        <button class="tab-link" data-tab-id="tab3">About me</button>
+      </div>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+    <body>
+      
+      <div class="tab-content-container">
+      <div id="tab1" class="tab-content" style="display: block;">
+        <h1>Hello and welcome to my portfolio!</h1>
+        <p>I'm a senior at University of Santa Cruz studying computer science game design. 
+        Throughout the years, I have worked on a wide varity of games. One of my proudest work is my capstone project, which is a WebGL build targeted for the mobile platform!
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+
+  
+        In that project, I focused on designing the game. My roles in the project were design and playtest lead. I also fill in any programming work when the production lead requires my assistance. 
+        </p>
+      </div>
+      </div>
+      <div id="tab2" class="tab-content">
+          <h1>Projects</h1>
+          <p>'m a senior at University<br>
+           of Santa Cruz studying<br>
+            computer science game design<br>. 
+          Throughout the years, <br>I have worked on a wide varity of <br>games. One of my proudest work is my capstone project, which is a WebGL build targeted for the mobile platform!
+      </div>
+      <div id="tab3" class="tab-content" >
+          <h1>About Me</h1>
+          <p>This is About Me</p>
+      </div>
+    </body>
   </div>
 `
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+document.addEventListener("DOMContentLoaded", function () {
+  // Get all tab links
+  const tabLinks = document.querySelectorAll(".tab-link");
+
+  // Add click event listeners to each tab link
+  tabLinks.forEach((tabLink) => {
+      tabLink.addEventListener("click", () => {
+          // Remove 'active' class from all tab links
+          tabLinks.forEach((link) => {
+              link.classList.remove("active");
+          });
+
+          // Add 'active' class to the clicked tab link
+          tabLink.classList.add("active");
+
+          // Hide all tab content
+          const tabContents = document.querySelectorAll(".tab-content");
+          tabContents.forEach((content) => {
+              (content as HTMLElement).style.display = "none";
+          });
+
+          // Show the corresponding tab content for the clicked link
+          const targetTabId = tabLink.getAttribute("data-tab-id");
+          if(targetTabId){
+            const targetTabContent = document.getElementById(targetTabId);
+            if (targetTabContent) {
+              targetTabContent.style.display = "block";
+            }
+          }
+          
+      });
+  });
+});
+
