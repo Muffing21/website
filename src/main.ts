b60fb081096905bd3c2d90c9ea4bc58806a5d824
wichapas <wpichetp@ucsc.edu>
2024-03-28 06:34:25 -0700

    website update

--- src/main.ts
+++ src/main.ts
@@ -3,57 +3,60 @@ import './style.css'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <div class="header">
-      <a href="https://github.com/Muffing21" target="_blank">
-        <img width="200" height="200" alt="GitHub Logomark" class="height-auto" src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png">
-      </a>
+      
       <h1>Wichapas Pichetpongsa</h1>
-      <div class="tabs">
+      <a href="https://github.com/Muffing21" target="_blank">
+        <img width="50" height="50" alt="GitHub Logomark" class="height-auto" src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png">
+      </a>
+      <a href="https://www.linkedin.com/in/wichapas-pichetpongsa-b17955211/" target="_blank">
+        <img width="50" height="50" class="height-auto" src="img/linkedin.jpg">
+      </a>
+      
+      
+    </div>
+    
+    <div class="tabs">
         <button class="tab-link active" data-tab-id="tab1">Home</button>
         <button class="tab-link" data-tab-id="tab2">Projects</button>
         <button class="tab-link" data-tab-id="tab3">About me</button>
-      </div>
-    </div>
+    </div>  
     <body>
-      
       <div class="tab-content-container">
-      <div id="tab1" class="tab-content" style="display: block;">
-        <h1>Hello and welcome to my portfolio!</h1>
-        <p>I'm a senior at University of Santa Cruz studying computer science game design. 
-        Throughout the years, I have worked on a wide varity of games. One of my proudest work is my capstone project, which is a WebGL build targeted for the mobile platform!
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
+        <div id="tab1" class="tab-content" style="display: block;">
+          <h1>Hello and welcome to my portfolio!</h1>
+          <p>I'm a senior at University of Santa Cruz studying computer science game design. 
+          Throughout the years, I have worked on a wide varity of games. One of my proudest work is my capstone project, which is a WebGL build targeted for the mobile platform!
+          In that project, I focused on designing the game. My roles in the project were design and playtest lead. I also fill in any programming work when the production lead requires my assistance.
+          <br>
+          <br>
+          Here is a link to download my resume! (for more details about projects please stay on this site)
+          <a href="img/Harry_Resume_2024.pdf" download>
+            <p>Download Here!</p>
+          </a>
+          <img src="img/resume_img.jpg" width="250" height="480">
+        </div>
 
-  
-        In that project, I focused on designing the game. My roles in the project were design and playtest lead. I also fill in any programming work when the production lead requires my assistance. 
-        </p>
+        <div id="tab2" class="tab-content">
+            <h1>Projects</h1>
+            <p>'m a senior at University<br>
+              of Santa Cruz studying<br>
+              computer science game design<br>. 
+            Throughout the years, <br>I have worked on a wide varity of <br>games. One of my proudest work is my capstone project, which is a WebGL build targeted for the mobile platform!
+        </div>
+        
+        <div id="tab3" class="tab-content" >
+            <h1>About Me</h1>
+            <img width="360" height="480" class="height-auto" src="img/IMG-6365.JPG">
+            </a>
+            <div class="text-color">
+              <p>Hello!</p>
+            </div>
+            <div class="body-text">
+              <p>I'm a senior at University of California Santa Cruz studying Computer Science: Computer Game Design.</p>
+            </div>
+        </div>
       </div>
-      </div>
-      <div id="tab2" class="tab-content">
-          <h1>Projects</h1>
-          <p>'m a senior at University<br>
-           of Santa Cruz studying<br>
-            computer science game design<br>. 
-          Throughout the years, <br>I have worked on a wide varity of <br>games. One of my proudest work is my capstone project, which is a WebGL build targeted for the mobile platform!
-      </div>
-      <div id="tab3" class="tab-content" >
-          <h1>About Me</h1>
-          <p>This is About Me</p>
-      </div>
-    </body>
+    </body>  
   </div>
 `
 document.addEventListener("DOMContentLoaded", function () {
